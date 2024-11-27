aef0c4b27273549d24ffd80af833c0e4f78b676d
khue <khue251103@gmail.com>
2024-11-27 15:11:00 +0700

    nothing

--- src/App.tsx
+++ src/App.tsx
@@ -10,47 +10,43 @@ import ProtectedRoute from './utils/ProtectedRoute';
 import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
-  const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
-
-  // return (
-  //   <BrowserRouter>
-  //     <Layout>
-  //       <Routes>
-  //         <Route
-  //           path="/"
-  //           element={<Navigate to="/products" />}
-  //         />
-  //         <Route
-  //           path="/login"
-  //           element={<Login />}
-  //         />
-  //         <Route
-  //           path="/products"
-  //           element={<Products />}
-  //         />
-  //         <Route
-  //           path="/cart"
-  //           element={<Cart />}
-  //         />
-  //       </Routes>
-  //     </Layout>
-  //   </BrowserRouter>
-  // );
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.auth.isAuthenticated,
+  );
 
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path="/" element={<Navigate to="/products" />} />
+          {/* Redirect root to products */}
+          <Route
+            path="/"
+            element={<Navigate to="/products" />}
+          />
+
+          {/* Redirect authenticated users from login to products */}
           <Route
             path="/login"
             element={isAuthenticated ? <Navigate to="/products" /> : <Login />}
           />
+
+          {/* Protected routes */}
           <Route element={<ProtectedRoute />}>
-            <Route path="/products" element={<Products />} />
-            <Route path="/cart" element={<Cart />} />
+            <Route
+              path="/products"
+              element={<Products />}
+            />
+            <Route
+              path="/cart"
+              element={<Cart />}
+            />
           </Route>
-          <Route path="*" element={<NotFoundPage />} />
+
+          {/* Catch-all route for 404 */}
+          <Route
+            path="/*"
+            element={<NotFoundPage />}
+          />
         </Routes>
       </Layout>
     </BrowserRouter>
